3099b4a4987f916135a2707072ff9ff28014876e
f4bian-pacheco <fabian.pacheco@haulmer.com>
2024-12-04 19:09:26 -0300

    feat: actualizar metadatos y mejorar la presentación del currículum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,19 @@ export default function Page() {
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
-              <BlurFadeText
+              {/* <BlurFadeText
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
                 text={`Hola, soy ${DATA.name} 👋`}
-              />
+              /> */}
+              <h1>
+                <BlurFadeText
+                  className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                  delay={BLUR_FADE_DELAY}
+                  text={`Hola, soy ${DATA.name} 👋`}
+                />
+              </h1>
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
                 delay={BLUR_FADE_DELAY}
